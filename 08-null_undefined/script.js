1a508a829033f256a2e71dd33a6c7b839fbabe44
Mateus Santos <mateussilvanzg@gmail.com>
2025-03-31 22:13:38 -0300

    feat: null and undefined

---
+++ 08-null_undefined/script.js
@@ -0,0 +1,23 @@
+var nome = null;
+console.log(nome);
+
+nome = "Mateus";
+console.log(nome);
+
+nome = undefined;
+console.log(nome);
+
+console.log("Idade: " + idade);
+
+// let idade = 20;
+// const idade = 20;
+var idade = 20;
+
+console.log("Fim");
+
+sayHello();
+
+function sayHello() {
+	console.log("Hello, Wolrd!");
+};
+
