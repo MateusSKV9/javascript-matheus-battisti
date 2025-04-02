f38020b14508e7cc9c9c3684da879a55dbc24434
Mateus Santos <mateussilvanzg@gmail.com>
2025-04-01 21:58:48 -0300

    feat: comparison operation

---
+++ 13-op_comparacao/script.js
@@ -0,0 +1,20 @@
+var idade = 15;
+
+if (idade >= 18) {
+	console.log("O usuário pode  fazer a carteira");
+}
+
+if (idade <= 17) {
+	console.log("O usuário não pode fazer a carteira");
+}
+
+var nome = "Mateus";
+if (nome.includes("Mat")) {
+	console.log("POssui a substring 'Mat'");
+} else {
+	console.log("Não possui a substring 'Mat'");
+}
+
+if (nome == "Mateus") {
+	console.log(`O nome é ${nome}`);
+}
