45ffb54204bdc03f311ce4b929b4beb1bed68f74
Mateus Santos <mateussilvanzg@gmail.com>
2025-04-01 21:57:24 -0300

    feat: if

---
+++ 12-if/script.js
@@ -0,0 +1,15 @@
+var idade = 61;
+
+console.log("Antes do if");
+
+if (idade < 13) {
+	console.log("Criança");
+} else if (idade < 18) {
+	console.log("Adolecente");
+} else if (idade < 60) {
+	console.log("Adulto");
+} else {
+	console.log("Idoso");
+}
+
+console.log("Depois do if");
