a64f09ad5ed1607a26792368859fa820c8ba161a
Mateus Santos <mateussilvanzg@gmail.com>
2025-04-14 21:16:48 -0300

    feat: and operator

---
+++ 15-operador_and/script.js
@@ -0,0 +1,14 @@
+var idade = 17;
+var nome = "Mateus";
+
+if (nome == "João" && idade == 16) {
+	console.log(`O nome dele é ${nome} e ${idade}`);
+}
+
+if (1 == 1 && 3 > 2 && true) {
+	console.log("Passou");
+}
+
+if (false && 1 == 1 && 3 > 2) {
+	console.log("Perdeu");
+}
