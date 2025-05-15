eb25704606d6cc0bfbed877de0aa3a2f9c71bb0a
Mateus Santos <mateussilvanzg@gmail.com>
2025-05-15 20:37:05 -0300

    feat: not

---
+++ 17-not/script.js
@@ -0,0 +1,13 @@
+let passou = false;
+console.log(passou);
+
+console.log(!passou);
+console.log("---");
+
+let valor = " ";
+let result = !valor;
+console.log(result);
+
+console.log("---");
+result = !!valor;
+console.log(result);
