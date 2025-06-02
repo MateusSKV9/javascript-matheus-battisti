6f90db906f6e1e38c10445ebd7b9622722cf39e4
Mateus Santos <mateussilvanzg@gmail.com>
2025-06-02 13:44:36 -0300

    feat: break and continue

---
+++ 21-break-continue/js/script.js
@@ -0,0 +1,12 @@
+for (var i = 100; i > 0; i--) {
+	if (i % 2 == 0) continue;
+
+	console.log(i);
+}
+
+console.log("/n");
+
+for (var j = 11; j <= 200; j++) {
+	if (j == 17) break;
+  console.log(j);
+}
